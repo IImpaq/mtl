bebf9c3a9834d19348304dd0a413408bd7c68907
Marcus Gugacs <iimpaq@proton.me>
2024-02-07 20:59:13 +0100

    Added byte datatype definition

---
+++ ntl/src/data/Byte.hpp
@@ -0,0 +1,15 @@
+/**
+* @file Byte.hpp
+* @author Marcus Gugacs
+* @date 02/07/2024
+* @copyright Copyright (c) 2024 Marcus Gugacs. All rights reserved.
+*/
+
+#ifndef NTL_BYTE_HPP
+#define NTL_BYTE_HPP
+
+namespace ntl {
+  typedef unsigned char Byte;
+}
+
+#endif // NTL_BYTE_HPP
