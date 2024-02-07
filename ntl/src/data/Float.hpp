c6f36145542cb11f8808e8b686505a3391e38360
Marcus Gugacs <iimpaq@proton.me>
2024-02-07 21:01:12 +0100

    Added float datatype definition

---
+++ ntl/src/data/Float.hpp
@@ -0,0 +1,15 @@
+/**
+* @file Float.hpp
+* @author Marcus Gugacs
+* @date 02/07/2024
+* @copyright Copyright (c) 2024 Marcus Gugacs. All rights reserved.
+*/
+
+#ifndef NTL_FLOAT_HPP
+#define NTL_FLOAT_HPP
+
+namespace ntl {
+  typedef float Float;
+}
+
+#endif // NTL_FLOAT_HPP
