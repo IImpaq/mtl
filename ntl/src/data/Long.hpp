27202b0cc50cc68270de9f30b87c4e1250afa09b
Marcus Gugacs <iimpaq@proton.me>
2024-02-07 21:02:39 +0100

    Added long datatype definitions

---
+++ ntl/src/data/Long.hpp
@@ -0,0 +1,19 @@
+/**
+* @file Long.hpp
+* @author Marcus Gugacs
+* @date 02/07/2024
+* @copyright Copyright (c) 2024 Marcus Gugacs. All rights reserved.
+*/
+
+#ifndef NTL_LONG_HPP
+#define NTL_LONG_HPP
+
+namespace ntl {
+  typedef long L32;
+  typedef long long L64;
+
+  typedef unsigned long UL32;
+  typedef unsigned long long UL64;
+}
+
+#endif // NTL_LONG_HPP
