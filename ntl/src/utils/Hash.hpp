94e99924c0d25c70c8228980918dc204e3a818ee
Marcus Gugacs <iimpaq@proton.me>
2024-02-08 09:04:05 +0100

    Added basic array implementation

--- ntl/src/utils/Hash.hpp
+++ ntl/src/utils/Hash.hpp
@@ -17,7 +17,7 @@ namespace ntl::hash {
   template <typename T, typename... Rest>
   void Combine(std::size_t& seed, const T& v, const Rest&... rest) {
     seed ^= std::hash<T>{}(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-    (HashCombine(seed, rest), ...);
+    (Combine(seed, rest), ...);
   };
 }
 
